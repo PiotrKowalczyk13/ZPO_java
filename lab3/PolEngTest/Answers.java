7ef0561235b23b537bbf14bc2202df7d4567637f
PiotrKowalczyk13 <62451050+PiotrKowalczyk13@users.noreply.github.com>
2020-04-06 22:35:35 +0200

    Delete Answers.java

--- lab3/PolEngTest/Answers.java
+++
@@ -1,9 +0,0 @@
-public class Answers {
-    private String original;
-    private String user_answer;
-
-    public Answers(String q, String ua){
-        this.original = q;
-        this.user_answer = ua;
-    }
-}
