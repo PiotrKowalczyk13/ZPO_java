3c3ad2de8fdcd08eebf2216001fdef0046355f91
PiotrKowalczyk13 <62451050+PiotrKowalczyk13@users.noreply.github.com>
2020-04-19 17:51:29 +0200

    Delete MAIN.java

--- lab5/ROZGRZEWKA/MAIN.java
+++
@@ -1,9 +0,0 @@
-public class MAIN {
-    public static void main(String[] args) {
-        int n = 10  ;
-        for(int i = 0; i < n; i++){
-            Threads threads = new Threads(i, n-1);
-            threads.start();
-        }
-    }
-}
